10dc2fc8912e290a05e046c3bcae1a77b9f6fb16
krlea_dev <alexcirlea@hotmail.com>
2025-03-26 15:04:36 +0200

    [ADDED] Error redirecting

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Failed to use the program: {err}");
+        eprintln!("Failed to use the program: {err}");
         process::exit(1);
     });
     if let Err(err) = lib::run(input_config) {
-        println!("Following error occurred: {err}");
+        eprintln!("Following error occurred: {err}");
         process::exit(1);
     };
 }
