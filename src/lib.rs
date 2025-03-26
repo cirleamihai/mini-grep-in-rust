603ed0e5a95cf829578bbfa951702150f2b86d2b
krlea_dev <alexcirlea@hotmail.com>
2025-03-26 13:53:43 +0200

    [ADDED] TDD Approach

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::error::Error;
+use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(config.file_text)?;
@@ -16,7 +16,7 @@ pub struct Config {
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("not enough arguments")
+            return Err("not enough arguments");
         }
 
         Ok(Config {
@@ -25,3 +25,32 @@ impl Config {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.
+        ";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+        let mut result = Vec::new();
+        for line in contents.lines() {
+            if line.contains(query) {
+                result.push(line.trim());
+            }
+        };
+
+        result
+    }
+}
+
+fn main() {}
